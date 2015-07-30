e2145a50f03fbecf4f502951bc778d73d3609c48
Ashish Shah <ashneo76@gmail.com>
2015-07-30 09:24:33 -0500

    Add logging to start/stop

--- generators/app/templates/_start.sh
+++ generators/app/templates/_start.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-python2 bot.py 2>&1 &> bot.log &
-echo $! > bot.pid
+PID_FILE=bot.pid
+if [ ! -f $PID_FILE ]; then
+    python2 bot.py 2>&1 &> bot.log &
+    PID=$!
+    echo $PID > $PID_FILE
+    echo "Bot started. PID: $PID"
+else
+    PID=$(cat $PID_FILE)
+    echo "Bot already started. PID: $PID"
+fi
--- generators/app/templates/_stop.sh
+++ generators/app/templates/_stop.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-PID=bot.pid
-if [ -f $PID ]; then
-    kill -9 $(cat $PID)
-    rm -rf $PID
+PID_FILE=bot.pid
+if [ -f $PID_FILE ]; then
+    PID=$(cat $PID_FILE)
+    kill -9 $PID
+    if [ $? -eq "0" ]; then
+        echo "Bot stopped. PID: $PID"
+        rm -rf $PID_FILE
+    else
+        echo "Failed to stop bot. PID: $PID"
+    fi
+else
+    echo "Bot already stopped."
 fi
